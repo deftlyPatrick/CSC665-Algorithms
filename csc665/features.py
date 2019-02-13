f29b6393e5562368bbb535350a8649c3f70378b9
deftlypatrick <wongpatrick98@gmail.com>
2019-02-13 02:02:15 -0800

    new

--- csc665/features.py
+++ csc665/features.py
@@ -1,23 +1,75 @@
-
+from sklearn.utils import shuffle
+import numpy as np 
+import pandas as pd
 
 
 def train_test_split(X, y, test_size, shuffle, random_state = NONE):
+	#Requirements
+
 	#X, y - features and the target variable
 	#test size - between 0 and 1 - how much to allocate to the test set; the rest goes to the train set. 
 	#shuffle - if True, shuffle the dataset, otherwise not. 
 	#random_state, integer; if None, the results are random, otherwise fixed to a given seed. 
 	#Example
+		#X_train & X_test = feature for the training and testing data
+		#y_train & y_train = labels for the training and testing data
+		#X_train, X_test, y_train, y_test = train_test_split(feat_df, y, 0.3, True, 12)
+	test
+
+	if shuffle = True:
+		shuffle(X, y)
+	else:
+		print("Shuffle is false.")
+
+	if random_state = NONE:	
+		seed = np.random(X,y)
+	else:
+
 
 
 create_catories(df, list_columns)
+	#Requirements
+
 	#converts values in place, in-place, in the columns passed in the list_columns to numerical values. 
 	#Follow the same approach: "string" -> category ->code.
 	#Replace values in df, in-place
 
-X,y = preprocess_ver_1_(csv_df)
+	#iterate through every row and column and change column -> int 
+	for list_columns in df.itertuples():
+		inPlace = df.pd.to_numeric(s, errors = 'coerce')
+
+
+
+
+
+
+X, y = preprocess_ver_1_(csv_df)
+	#Requirements
+
 	#Apply the feature transformation steps to the dataframe, return new x and y for the entire dataset. Do not modify the original csv_df.
 	#Remove all rows with NA values
-	#Convert datatime to a number
+	#Convert datetime to a number
 	#Convert all strings to numbers
 	#Split the dataframe into X and y and return these. 
 
+	#Apply the feature transformation steps to the dataframe, 
+	#return new x and y for the entire dataset. Do not modify the original csv_df.
+	df = pd.DataFrame(data = {'X': X, 'y': y}
+	df.transform(X, y)
+
+	#Remove all rows with NA values
+	index = np.arange(X, y)
+	csv_df = pd.DataFrame(index = index)
+
+	#Convert datetime to a number
+	csv_df['Date'] = pd.to_datetime(csv_df['Date'], infer_datetime_format = True)
+	csv_df['Date'] = csv_df['Date'].astype(np.int64)
+
+	#Convert all strings to numbers
+
+
+	#Split the dataframe into X and y and return these.
+
+
+
+	
--- csc665/metrics.py
+++ csc665/metrics.py
@@ -1,8 +1,15 @@
+import numpy as np 
+
 def mse(y_predicted, y_true)
 	#return Mean-Squared Error
+	#((y_hat - y_test) ** 2).mean() -> using numpy 
+	return np.square(np.subtract(y_predicted - y_true)).mean()
 
 def rmse(y_predicted, y_true)
 	#return Root Mean-Squared Error
+	return np.sqrt(np.square(np.subtract(y_predicted - y_true)).mean())
+
 
 def rsq(y_predicted, y_true)
 	#return R^2 
+	return np.square(y_predicted), np.square(y_true)
