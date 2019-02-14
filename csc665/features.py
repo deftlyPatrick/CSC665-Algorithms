2de81ec6eafea349d7603f6782a014f97565b33b
deftlypatrick <wongpatrick98@gmail.com>
2019-02-14 04:13:45 -0800

    new

--- csc665/features.py
+++ csc665/features.py
@@ -1,86 +1,61 @@
-from sklearn.utils import shuffle
-import numpy as np 
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.ensemble import RandomForestRegressor
+rf = RandomForestRegressor()
+csv_df = pd.read_csv("Melbourne_housing_FULL.csv")
+csv_df.head()
+
+def train_test_split(X, y, test_size, shuffle, random_state = None):
+
+    return X_train, X_test, y_train, y_test
+
+def create_categories(df, list_columns):
+    for i in range(len(list_columns)):
+        lst = list_columns[i]
+        df[lst] = df[lst].astype('category').cat.codes
+    return df
+
+def preprocess_ver_1(csv_df):
+    # csv_df = pd.read_csv(csv_file)
+    # feat_df = csv_df.drop('Price', axis=1)
+    # y = csv_df['Price'].values
+
+    feat_df = csv_df.drop('Price', axis = 1)
+    csv_df.shape
+    feat_df.shape
+    y = csv_df['Price'].values
+    y.shape
+    rows_labeled_na = csv_df.isnull().any(axis=1)
+    rows_with_na = csv_df[rows_labeled_na]
+    rows_with_data = csv_df[~rows_labeled_na]
+    csv_df.shape, rows_with_na.shape, rows_with_data.shape
+    feat_df = rows_with_data.drop('Price', axis=1)
+    feat_df.shape
+    y = rows_with_data['Price'].values
+    y.shape
+    suburbs = {}
+    for s in feat_df["Suburb"].values:
+        if s not in suburbs:
+            suburbs[s] = len(suburbs)
+    feat_df['Suburb'] = feat_df['Suburb'].replace(suburbs)
+    categories_list = ['Type', 'Address', 'Method', 'SellerG', 'CouncilArea', 'Regionname']
+    feat_df = create_categories(feat_df, categories_list)
+    feat_df.head()
+    feat_df['Date'] = pd.to_datetime(feat_df['Date'], infer_datetime_format=True)
+    feat_df['Date'] = feat_df['Date'].astype(np.int64)
+    X = feat_df['Date'].values
+    feat_df.head()
+    rf.fit(feat_df, y)
+    return X, y
 
 
-def train_test_split(X, y, test_size, shuffle, random_state = NONE):
-	#Requirements
+x, y = preprocess_ver_1(csv_df)
 
-	#X, y - features and the target variable
-	#test size - between 0 and 1 - how much to allocate to the test set; the rest goes to the train set. 
-	#shuffle - if True, shuffle the dataset, otherwise not. 
-	#random_state, integer; if None, the results are random, otherwise fixed to a given seed. 
-	#Example
-		#X_train & X_test = feature for the training and testing data
-		#y_train & y_train = labels for the training and testing data
-		#X_train, X_test, y_train, y_test = train_test_split(feat_df, y, 0.3, True, 12)
+plt.scatter(y, x)
+plt.show()
 
-
-	#test size - between 0 and 1 - how much to allocate to the test set; the rest goes to the train set. 
-	if test_size > 1: 
-		print("Error: Test Size is too big")
-	elif test_size < 0:
-		print("Error: Test Size is too small")
-	else:
-		test_set = 1 - test_size
-		train_set = test_size
-
-	#shuffle - if True, shuffle the dataset, otherwise not. 
-	if shuffle = True:
-		shuffle(X, y)
-	else:
-		print("Shuffle is false.")
-
-	#random_state, integer; if None, the results are random, otherwise fixed to a given seed. 
-	if random_state = NONE:	
-		seed = np.random(X,y)
-	else:
-
-
-
-create_catories(df, list_columns)
-	#Requirements
-
-	#converts values in place, in-place, in the columns passed in the list_columns to numerical values. 
-	#Follow the same approach: "string" -> category ->code.
-	#Replace values in df, in-place
-
-	#iterate through every row and column and change column -> int 
-	for list_columns in df.itertuples():
-		inPlace = df.pd.to_numeric(s, errors = 'coerce')
-
-
-
-
-
-
-X, y = preprocess_ver_1_(csv_df)
-	#Requirements
-
-	#Apply the feature transformation steps to the dataframe, return new x and y for the entire dataset. Do not modify the original csv_df.
-	#Remove all rows with NA values
-	#Convert datetime to a number
-	#Convert all strings to numbers
-	#Split the dataframe into X and y and return these. 
-
-	#Apply the feature transformation steps to the dataframe, 
-	#return new x and y for the entire dataset. Do not modify the original csv_df.
-	df = pd.DataFrame(data = {'X': X, 'y': y}
-	df.transform(X, y)
-
-	#Remove all rows with NA values
-	index = np.arange(X, y)
-	csv_df = pd.DataFrame(index = index)
-
-	#Convert datetime to a number
-	csv_df['Date'] = pd.to_datetime(csv_df['Date'], infer_datetime_format = True)
-	csv_df['Date'] = csv_df['Date'].astype(np.int64)
-
-	#Convert all strings to numbers
-
-
-	#Split the dataframe into X and y and return these.
-
-
-
-	
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(x, y)
+x.shape, X_train.shape, X_test.shape, y_train.shape, y_test.shape
