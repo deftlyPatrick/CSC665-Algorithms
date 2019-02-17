09f8821eaadd84684f0e2298933b8ea46a698be7
deftlypatrick <wongpatrick98@gmail.com>
2019-02-16 22:53:45 -0800

    new

--- csc665/features.py
+++ csc665/features.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pandas as pd
 from sklearn import utils
+import random
 from sklearn.ensemble import RandomForestRegressor
 rf = RandomForestRegressor()
 csv_df = pd.read_csv("Melbourne_housing_FULL.csv")
 csv_df.head()
+
 
 def train_test_split(X, y, test_size, shuffle, random_state = None):
 
@@ -13,23 +15,37 @@ def train_test_split(X, y, test_size, shuffle, random_state = None):
     #Random state only used in shuffle
 
     if shuffle is True:
-        X = utils.shuffle(X, n_samples=len(X), random_state=random_state)
-        y = utils.shuffle(y, n_samples=len(y), random_state=random_state)
+        indices = np.arange(X.shape[0])
+        random_index = utils.shuffle(indices, n_samples=len(indices), random_state=random_state)
+        shuffle_x = X[random_index]
+        shuffle_y = y[random_index]
+        # print(shuffle_x)
+        # print(shuffle_y)
 
+    # Multiply the test_size to the length of x to determine how many number to split between test and train
+    x_size = round(len(shuffle_x) * test_size)
 
-    trainX = round(len(X) * (1 - test_size))
-    testX = round(len(X) * test_size)
+    X_train = shuffle_x[:-x_size]
+    X_test = shuffle_x[-x_size:]
 
-    X_train = X[trainX:]
-    X_test = X[:testX]
-
-    trainY = round(len(y) * (1 - test_size))
-    testY = round(len(y) * test_size)
-
-    y_train = y[trainY:]
-    y_test = y[:testY]
+    y_train = shuffle_y[:-x_size]
+    y_test = shuffle_y[-x_size:]
 
     return X_train, X_test, y_train, y_test
+
+
+def shuffle_data(X, y):
+    x_length = len(X)
+    for i in range(x_length-1):
+        swap_data(X, y, i, random.randrange(i, x_length))
+        return X, y
+
+
+def swap_data(X, y, index, random_index):
+    # Swap the index and replace with random index
+    X[index], X[random_index] = X[random_index], X[index]
+    y[index], y[random_index] = y[random_index], y[index]
+
 
 
 def create_categories(df, list_columns):
@@ -38,12 +54,13 @@ def create_categories(df, list_columns):
         df[lst] = df[lst].astype('category').cat.codes
     return df
 
+
 def preprocess_ver_1(csv_df):
     # csv_df = pd.read_csv(csv_file)
     # feat_df = csv_df.drop('Price', axis=1)
     # y = csv_df['Price'].values
 
-    X = csv_df.drop('Price', axis = 1)
+    X = csv_df.drop('Price', axis=1)
     csv_df.shape
     X.shape
     y = csv_df['Price'].values
@@ -73,28 +90,37 @@ def preprocess_ver_1(csv_df):
 
 
 X, y = preprocess_ver_1(csv_df)
-print(X)
-print(y)
 
 
 
-# rf = RandomForestRegressor(n_estimators=1 , random_state=17)
-# rf = RandomForestRegressor(n_estimators=5, random_state=17)
-# rf = RandomForestRegressor(n_estimators=10 , random_state=17)
-# rf = RandomForestRegressor(n_estimators=20, random_state=17)
-# rf = RandomForestRegressor(n_estimators=40, random_state=17)
-# rf = RandomForestRegressor(n_estimators=80, random_state=17)
-# rf = RandomForestRegressor(n_estimators=160, random_state=17)
-# rf = RandomForestRegressor(n_estimators=200, random_state=17)
+X_train1, X_test1, y_train1, y_test1 = train_test_split(X, y, 0.25, True, 5)
+print(X_train1)
+print(X_test1)
+print(y_train1)
+print(y_test1)
+print("\n")
+X_train2, X_test2, y_train2, y_test2 = train_test_split(X, y, 0.25, True, 5)
+print(X_train2)
+print(X_test2)
+print(y_train2)
+print(y_test2)
+print("\n")
+X_train3, X_test3, y_train3, y_test3 = train_test_split(X, y, 0.25, True, None)
+print(X_train3)
+print(X_test3)
+print(y_train3)
+print(y_test3)
+print("\n")
+X_train4, X_test4, y_train4, y_test4 = train_test_split(X, y, 0.25, True, None)
+print(X_train4)
+print(X_test4)
+print(y_train4)
+print(y_test4)
+print("\n")
 
-
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, 0.25, True)
-print(X_train)
-print(X_test)
-print(y_train)
-print(y_test)
+# X_train, X_test, y_train, y_test = train_test_split(x_1, y_1, 0.25, True, None)
 
 
 
 # X.shape, X_train.shape, X_test.shape, y_train.shape, y_test.shape
+
